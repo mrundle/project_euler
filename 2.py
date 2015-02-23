70800e32184c205f91e1081b5d39e9c3466bb38f
mrundle <mrundle@nd.edu>
2015-02-22 21:05:17 -0500

    Update 2nd solution so that it runs, oops.

--- 2.py
+++ 2.py
@@ -1,16 +1,16 @@
 # returns sum of even fibonacci numbers
 # under a max threshold (not including seeds)
-def even_fibs(a, b, max):
+def sum_even_fibs(a, b, max):
   if a + b > max:
     return 0
   else:
     add = 0
     if (a + b) % 2 == 0:
       add = a + b
-    return next_fib(b, a + b, max) + add
+    return sum_even_fibs(b, a + b, max) + add
 
 if __name__ == "__main__":
   upper_limit = 4000000
   # 2 must be added because the function doesn't include
   # seed values in its calculation
-  print 2 + next_fib(1, 2, upper_limit)
+  print 2 + sum_even_fibs(1, 2, upper_limit)
