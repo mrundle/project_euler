3b6d8209712e66ddc483486ad7900bac69f520d3
Matt Rundle <mrundle@nd.edu>
2015-02-23 10:16:54 -0500

    solution for 8

--- 8.py
+++ 8.py
@@ -1,31 +1,44 @@
+# Problem 8
+# ProjectEuler.net
+#
+# Largest product in a series
 
+def increment_idx(start_idx, end_idx, num_adj):
+  if (end_idx - start_idx) < (num_adj - 1):
+    end_idx += 1
+  else:
+    end_idx += 1
+    start_idx += 1
+  return start_idx, end_idx
 
-# Idea:
-#   1. read the first 13 digits
-#   2. move procedurally
-#   3. if at any point a zero is encountered,
-#      disregard all associated combinations
-def greatest_adj_prod(input, num_adj):
+def calculate_product(start_idx, end_idx, num_adj, input_str):
+  product = 1
+  for i in range(start_idx, end_idx + 1):
+    product *= int(input_str[i])
+  return product
+
+def greatest_adj_prod(input_str, num_adj):
   greatest = 0
   curr_prod = 1
   start_idx = 0
   end_idx = 0
-  while (end_idx - start_idx) < (num_adj - 1):
-    end_idx += 1
-    if end_idx > len(input):
-      return greatest
-    else if input[end_idx] == 0:
+  done = False
+  while not done:
+    start_idx, end_idx = increment_idx(start_idx, end_idx, num_adj)
+    if end_idx > (len(input_str) - 1):
+      done = True
+    elif input_str[end_idx] == 0:
       start_idx = end_idx + 1
       end_idx = start_idx
-    else
-      curr_prod *= input[end_idx]
-  
-  
+    elif (end_idx - start_idx) == (num_adj - 1):
+      curr_prod = calculate_product(start_idx, end_idx, num_adj, input_str)
+      if curr_prod > greatest:
+        greatest = curr_prod
  
   return greatest
     
 if __name__ == "__main__":
-  input = '73167176531330624919225119674426574742355349194934' \
+  input_str = '73167176531330624919225119674426574742355349194934' \
         '96983520312774506326239578318016984801869478851843' \
         '85861560789112949495459501737958331952853208805511' \
         '12540698747158523863050715693290963295227443043557' \
@@ -46,4 +59,5 @@ if __name__ == "__main__":
         '05886116467109405077541002256983155200055935729725' \
         '71636269561882670428252483600823257530420752963450' \
         
-  print greatest_adj_prod(input,13)
+  print greatest_adj_prod(input_str,13)
+  
