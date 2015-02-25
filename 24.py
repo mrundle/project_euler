a120b29f3a8a5b35f8946252122f8ca1928ca1d4
Matt Rundle <mrundle@nd.edu>
2015-02-25 18:46:37 -0500

    24 skeleton, probably going to need to study elsewhere for this one. good problem

---
+++ 24.py
@@ -0,0 +1,12 @@
+# REF: http://blog.bjrn.se/2008/04/lexicographic-permutations-using.html
+
+def it_through(seq, used):
+  if len(seq) == 0:
+    
+
+if __name__ == "__main__":
+
+  seq = [0,1,2]
+
+  get_perms(seq)
+  
